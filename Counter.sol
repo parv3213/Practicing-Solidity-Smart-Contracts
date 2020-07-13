889bba949780e1306dc656bb042735390eea4d6e
parv3213 <parv3213@gmail.com>
2020-07-13 07:54:19 +0530

    modification in Count.sol

--- Counter.sol
+++ Counter.sol
@@ -1,17 +1,9 @@
 pragma solidity ^0.6.0;
 
-contract Counter{
-    uint count;
-    
-    constructor() public {
-        count = 0;
-    }
-    
-    function getCount() public view returns(uint){
-        return count;
-    }
-    
+contract Counter {
+    uint256 public count = 1;
+
     function incrementCount() public {
-        count = count +1;
+        count = count + 1;
     }
 }
